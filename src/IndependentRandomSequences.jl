2e5c23c6dde792cd46acf0e7ada300bd9ed5aa41
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-08-15 00:40:51 -0700

    Initial funtionality and tests for UnivariateContinuous distributions

--- src/IndependentRandomSequences.jl
+++ src/IndependentRandomSequences.jl
@@ -1,5 +1,56 @@
 module IndependentRandomSequences
 
-# package code goes here
+using Distributions
+
+import Base: length
+import Distributions: _rand!,_logpdf,
+                      mean,var,cov,entropy
+
+export IIDRandomSequence,INIDRandomSequence
+
+#abstract AbstractINIDRandomSequence
+
+type IIDRandomSequence{T<:ContinuousUnivariateDistribution} <: ContinuousMultivariateDistribution
+  d::T
+  length::Int64
+end
+
+length(X::IIDRandomSequence) = X.length
+
+_rand!{T<:Real}(X::IIDRandomSequence, x::AbstractVector{T}) = rand!(X.d,x)
+
+function _logpdf{T<:Real}(X::IIDRandomSequence, x::AbstractVector{T})
+  return sum(x_i->logpdf(X.d,x_i),x)
+end
+
+mean(X::IIDRandomSequence) = mean(X.d)*ones(length(X))
+var(X::IIDRandomSequence) = var(X.d)*ones(length(X))
+cov(X::IIDRandomSequence) = Diagonal(var(X))
+entropy(X::IIDRandomSequence) = entropy(X.d)*length(X)
+
+
+
+type INIDRandomSequence <: ContinuousMultivariateDistribution
+    distributions::Vector
+end
+
+length(X::INIDRandomSequence) = length(X.distributions)
+
+function _rand!{T<:Real}(X::INIDRandomSequence, x::AbstractVector{T})
+  for (i,d_i) in enumerate(X.distributions)
+    x[i] = rand(d_i)
+  end
+  return x
+end
+
+function _logpdf{T<:Real}(X::INIDRandomSequence, x::AbstractVector{T})
+  return sum(arg->logpdf(arg[1],arg[2]),zip(X.distributions,x))
+end
+
+mean(X::INIDRandomSequence) = [mean(d_i) for d_i in X.distributions]
+var(X::INIDRandomSequence) = [var(d_i) for d_i in X.distributions]
+cov(X::INIDRandomSequence) = Diagonal(var(X))
+entropy(X::INIDRandomSequence) = sum(entropy,X.distributions)
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,27 @@
+using Distributions
 using IndependentRandomSequences
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+
+#IID tests
+W = Uniform(-1,1)
+N = 2
+Y = IIDRandomSequence(W,N)
+
+@test pdf(Y,[0., 0.]) == 1/4
+@test pdf(Y,[2., 0.]) == 0.
+@test pdf(Y,[0. 0. ; 2. 0.]') == [1/4, 0.]
+
+@test mean(Y) == [0., 0.]
+@test var(Y) == [1/3, 1/3]
+
+#INID tests
+W,X = Uniform(-1,1),Uniform(0,1)
+Y = INIDRandomSequence([W,X])
+
+@test pdf(Y,[0., 1/2]) == 1/2
+@test pdf(Y,[2., 1/2]) == 0.
+@test pdf(Y,[0. 1/2;  2. 0.]') == [1/2, 0.]
+
+@test mean(Y) == [0., 1/2]
+@test var(Y) == [1/3, 1/12]
